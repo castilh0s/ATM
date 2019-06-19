4cdc22d45a3d724029ff215e3f689d914eb3bda0
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-06-19 18:03:25 -0300

    :sparkles: Metódo pra atualizar valor total do ATM

--- atm/atm.py
+++ atm/atm.py
@@ -4,4 +4,9 @@ class ATM:
 
     def set_note(self, note, quantity):
         self.notes[note] = quantity
-        self.amount += (note * quantity)
+        self.update_amount()
+
+    def update_amount(self):
+        self.amount = 0
+        for key, value in self.notes.items():
+            self.amount += key * value
